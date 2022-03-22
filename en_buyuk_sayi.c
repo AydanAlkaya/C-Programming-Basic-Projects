10a486730baab14458567eef907567c4ae6998bb
Aydan <56941797+AydanAlkaya@users.noreply.github.com>
2022-03-22 23:38:10 +0300

    Add en_buyuk_sayi.c file

---
+++ en_buyuk_sayi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Girilen sayýlarýn en büyüðünü bulan,
+// 0 dan küçük deðer girildiðinde sonlandýrýlan fonksiyon.
+
+int main(void) {
+	int buyuk = -1, sayi = 0, flag = 0;
+	
+	printf("Bir sayi giriniz -> ");
+	scanf("%d", &sayi);
+	
+	while(sayi > 0){
+		flag = 1;
+		
+		if (sayi > buyuk)
+		  buyuk = sayi;
+		  
+		printf("\nBir sayi giriniz -> ");
+		scanf("%d", &sayi);}
+	
+	if (flag == 0)
+	   printf("\nHerhangi bir sayi girisi yapilmamistir.\n\n");
+    else
+	   printf("\nSu ana kadar girilen en buyuk sayi = %d\n\n", buyuk);
+	   	   
+	return 0;
+}
