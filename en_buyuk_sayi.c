51d4e693632e6e35ed0aed16dc1cd6bcea1f7bc4
Aydan <56941797+AydanAlkaya@users.noreply.github.com>
2022-03-23 00:01:16 +0300

    Fix indents in en_buyuk_sayi.c

--- en_buyuk_sayi.c
+++ en_buyuk_sayi.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Girilen sayýlarýn en büyüðünü bulan,
-// 0 dan küçük deðer girildiðinde sonlandýrýlan fonksiyon.
+// Girilen sayilarin buyugunu bulan,
+// 0 dan kucuk deger girildiginde sonlandirilan fonksiyon.
 
 int main(void) {
 	int buyuk = -1, sayi = 0, flag = 0;
@@ -10,19 +10,22 @@ int main(void) {
 	printf("Bir sayi giriniz -> ");
 	scanf("%d", &sayi);
 	
-	while(sayi > 0){
+	while(sayi > 0)
+	{
 		flag = 1;
 		
 		if (sayi > buyuk)
-		  buyuk = sayi;
-		  
+			buyuk = sayi;
+
 		printf("\nBir sayi giriniz -> ");
-		scanf("%d", &sayi);}
+		scanf("%d", &sayi);
+	}
+		
 	
 	if (flag == 0)
-	   printf("\nHerhangi bir sayi girisi yapilmamistir.\n\n");
-    else
-	   printf("\nSu ana kadar girilen en buyuk sayi = %d\n\n", buyuk);
-	   	   
+		printf("\nHerhangi bir sayi girisi yapilmamistir.\n\n");
+	else
+		printf("\nSu ana kadar girilen en buyuk sayi = %d\n\n", buyuk);
+			
 	return 0;
 }
